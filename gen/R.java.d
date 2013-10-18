09facc261683bb43432de65568d5ae530a8e7531
root <root@manuel-ESPRIMO-P.(none)>
2013-10-18 18:30:42 +0200

    pujari

--- gen/R.java.d
+++ gen/R.java.d
@@ -5,4 +5,8 @@
 /home/manuel/AndroidStudioProjects/MyfirstApp/res/drawable-mdpi/ic_launcher.png \
 /home/manuel/AndroidStudioProjects/MyfirstApp/res/values/strings.xml \
 /home/manuel/AndroidStudioProjects/MyfirstApp/res/layout/main.xml \
+/home/manuel/AndroidStudioProjects/MyfirstApp/bin/res/drawable-xhdpi/ic_launcher.png \
+/home/manuel/AndroidStudioProjects/MyfirstApp/bin/res/drawable-ldpi/ic_launcher.png \
+/home/manuel/AndroidStudioProjects/MyfirstApp/bin/res/drawable-hdpi/ic_launcher.png \
+/home/manuel/AndroidStudioProjects/MyfirstApp/bin/res/drawable-mdpi/ic_launcher.png \
 /home/manuel/AndroidStudioProjects/MyfirstApp/bin/AndroidManifest.xml \
